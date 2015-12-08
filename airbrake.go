9986fbf55d1d312bc2995de5ded8ad5dd450ab81
Parag Patel <parag.patel.one@gmail.com>
2015-12-08 12:47:19 -0600

    Fixed SendNoticeAsync error

--- airbrake.go
+++ airbrake.go
@@ -3,8 +3,6 @@ package airbrake
 import (
 	"errors"
 	"fmt"
-	"os"
-
 	"github.com/Invoiced/logrus"
 	"gopkg.in/airbrake/gobrake.v2"
 )
@@ -48,9 +46,7 @@ func (hook *airbrakeHook) Fire(entry *logrus.Entry) error {
 }
 
 func (hook *airbrakeHook) sendNotice(notice *gobrake.Notice) {
-	if _, err := hook.Airbrake.SendNoticeAsync(notice); err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to send error to Airbrake: %v\n", err)
-	}
+	hook.Airbrake.SendNoticeAsync(notice)
 }
 
 func (hook *airbrakeHook) Levels() []logrus.Level {
